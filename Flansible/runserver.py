255fa4d82f2a62b8c67c9f309f5cebaadac36bb1
123875@centrala.bzwbk <wojciech.mitus@bzwbk.pl>
2020-02-20 12:12:16 +0100

    Improve Flansible security by forcing REST API clients to use TLS 1.2

--- Flansible/runserver.py
+++ Flansible/runserver.py
@@ -1,5 +1,8 @@
+#!/bin/python3.6
 from flansible import app, config
 import platform
+import ssl
+
 #Visual studio remote debugger
 if platform.node() == 'mgmt':
     try:
@@ -8,5 +11,10 @@ if platform.node() == 'mgmt':
     except:
         pass
 
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('/usr/local/etc/flansible/flansible_cert.cer', '/usr/local/etc/flansible/flansible_privkey.pem')
+context.protocol = ssl.OP_NO_TLSv1 # prevents TLS 1.0
+context.protocol = ssl.OP_NO_TLSv1_1 # prevents TLS 1.1
+
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=False, port=int(config.get("Default", "Flask_tcp_port")))
+    app.run(ssl_context=context, debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=False, port=int(config.get("Default", "Flask_tcp_port")))

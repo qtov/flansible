e35e6fb7be0bcf4226112b986b079761cfa836c8
123875@centrala.bzwbk <wojciech.mitus@bzwbk.pl>
2020-03-20 09:25:16 +0100

    Turn off 3DES, turn off noauth ciphers

--- Flansible/runserver.py
+++ Flansible/runserver.py
@@ -15,6 +15,8 @@ context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('/usr/local/etc/flansible/flansible_cert.cer', '/usr/local/etc/flansible/flansible_privkey.pem')
 context.protocol = ssl.OP_NO_TLSv1 # prevents TLS 1.0
 context.protocol = ssl.OP_NO_TLSv1_1 # prevents TLS 1.1
+context.set_ciphers('HIGH:!3DES:!aNULL')
+
 
 if __name__ == '__main__':
     app.run(ssl_context=context, debug=True, host=config.get("Default", "Flask_tcp_ip"), use_reloader=False, port=int(config.get("Default", "Flask_tcp_port")))
